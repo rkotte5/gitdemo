7079630291f8c2e54f44d3a60354117021a66760
Rahul <rahul@123.com>
2023-01-22 17:31:07 +0530

    third comment

--- project/src/plateform/Test.java
+++ project/src/plateform/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main (String args [])
 	{
 		System.out.println("this is first line");
-		int a=15;
+		int a=20;
 		System.out.println(a*a);
+		System.out.println("this is end");
 	}
 }
